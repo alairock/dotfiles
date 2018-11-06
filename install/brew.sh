9728552117abdc9310ba3c1132bb8d525fc09cfc
Skyler Lewis <skyler.lewis@canopytax.com>
2018-11-06 14:01:39 -0700

    rm teamocil

--- install/brew.sh
+++ install/brew.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-
-gem install teamocil
-
 if test ! $(which brew); then
     echo "Installing homebrew"
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
